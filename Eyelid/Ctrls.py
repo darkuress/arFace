5274dd44d73e0c26728b82a79d60e3f3ec9fddb4
darkuress <jhwang.ani@gmail.com>
2017-06-07 22:47:32 -0700

    fixed eyelid ctl color

--- Eyelid/Ctrls.py
+++ Eyelid/Ctrls.py
@@ -201,7 +201,7 @@ class Ctrls(Func.Func, Base.Base):
                     #- center ctrl.tx drives ctlCrv center cv[2].xValue 
                     rCntMult = cmds.shadingNode('multiplyDivide', asUtility=True, n = uploPrefix +'Cnt_mult')
                     cmds.connectAttr(uploPrefix + "Center" + self.ctlSuffix + ".tx" , rCntMult + ".input1X") 
-                    #cmds.setAttr(rCntMult + ".input2X", -1)
+                    cmds.setAttr(rCntMult + ".input2X", -1)
                     cmds.connectAttr(rCntMult + ".outputX", cntAddD + ".input1")
                     cmds.setAttr(cntAddD + ".input2", 0.5) 
                     cmds.connectAttr(cntAddD + ".output" , ctlCrvCv[2] + ".xValue")            
@@ -368,7 +368,7 @@ class Ctrls(Func.Func, Base.Base):
             #- creating controller 
             if lr == self.prefix[0]:
                 cColor = 6
-            else:
+            elif lr == self.prefix[1]:
                 cColor = 17
             childPos = cmds.xform(cn + "Lid" + self.jntSuffix, t = True, q = True, ws = True)
             lidCtl, lidCtlP  = self.circleController(cn + "Lid" + self.ctlSuffix,
