6841a6bd8f69ae70303e0bd189469ae12adf049c
darkuress <jhwang.ani@gmail.com>
2017-06-07 22:21:07 -0700

    fixed r eyelid ctl movement

--- Eyelid/Func.py
+++ Eyelid/Func.py
@@ -388,7 +388,7 @@ class Func(Base.Base):
                 cmds.connectAttr(txForJnt + '.output', rotY_mult + '.input1X')
                 if self.prefix[1] in UD:
                     rotY_mult_inv = cmds.shadingNode('multiplyDivide', asUtility=True, n = UD + 'BlinkRY_mult_inv' + str(i+1).zfill(2))
-                    cmds.setAttr(rotY_mult_inv + '.input1X', -1)
+                    cmds.setAttr(rotY_mult_inv + '.input1X', 1)
                     cmds.connectAttr(self.faceFactors['eyelid'] + '.lidRotateY_scale', rotY_mult_inv + '.input2X')            
                     cmds.connectAttr(rotY_mult_inv + '.outputX', rotY_mult + '.input2X')
                 else:
